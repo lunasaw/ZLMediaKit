a4ad9373305a0749dadc55805c3c56e61ee638eb
guangdong.liao <liaoguangdong@weidian.com>
2023-11-20 11:24:08 +0800

    新增IPAddress

--- server/FileScanner.cpp
+++ server/FileScanner.cpp
@@ -352,31 +352,4 @@ std::vector<std::shared_ptr<Scanner::Info>> Scanner::getMediaInfo(std::string di
     return myfiles;
 }
 
-std::string IPAddress::getIP(const std::string &url) {
-    std::regex ipRegex(R"((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))");
-    std::smatch match;
-
-     if (std::regex_search(url, match, ipRegex)) {
-        std::string ip = match[1].str();
-        return ip;
-     }
-     return "";
 }
-
-bool IPAddress::isIPReachable(const std::string &ip) {
-    if (!ip.empty()) {
-        std::string command = "ping -c 1 " + ip;
-        int result = std::system(command.c_str());
-        InfoL << "ping:" << command << ",result:" << !result;
-        // 根据返回值判断是否可联通
-        if (result == 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    return false;
-}
-
-}
-
